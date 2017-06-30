fcaddf47e3a0d0792688ac5c8f19cfb8e86006c9
Matteo Basso <mbasso96@gmail.com>
2017-06-30 16:02:59 +0200

    regex, methods, fix cors

--- src/index.js
+++ src/index.js
@@ -15,19 +15,50 @@ const init = (
 ) => {
   const app = express();
 
+  app.all('*', (req, res, next) => {
+    const origin = req.get('Origin');
+    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+  });
+
   fs.readdirSync(directory).forEach((file) => {
     if (!/\.json$/.test(file)) return;
 
     const source = fs.readFileSync(path.join(directory, file), 'utf8');
-    const { url, method = 'get', status = 200, body } = JSON.parse(source);
+    const {
+      url,
+      regex,
+      method = 'get',
+      methods,
+      status = 200,
+      body,
+    } = JSON.parse(source);
 
-    app[method.toLowerCase()](url, (req, res) => {
-      res.status(status);
-      if (body !== undefined) {
-        res.setHeader('Content-Type', 'application/json');
-        res.send(JSON.stringify(body));
+    (methods || [method]).forEach((verb) => {
+      let listenTo;
+
+      if (typeof url === 'string') {
+        listenTo = url;
+      } else if (typeof regex === 'string') {
+        listenTo = new RegExp(regex);
+      } else {
+        listenTo = '';
       }
-      res.send();
+
+      console.log(`Adding ${verb} route: ${listenTo}`);
+
+      app[verb.toLowerCase()](listenTo, (req, res) => {
+        res.status(status);
+        if (body !== undefined) {
+          res.setHeader('Content-Type', 'application/json');
+          res.send(JSON.stringify(body));
+          return;
+        }
+        res.send();
+      });
     });
   });
 
